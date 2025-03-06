b71b9fd4627a9a5799d1928a2eb8bb0798ae705c
krishnj16 <jitanshukrishn@gmail.com>
2025-03-06 12:49:29 +0530

    Delete Dice.js

--- Dice.js
+++
@@ -1,9 +0,0 @@
-let x = Math.random();
-let random = Math.floor((x * 6) + 1);
-let randice = "dice" + random + ".png";
-let psource = "c:/Users/Krishna/Desktop/MERN/" + randice;  
-
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", psource);
-
-
