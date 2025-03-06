ca63a43a7e0a4f0050f494a46c789e4729406946
krishnj16 <jitanshukrishn@gmail.com>
2025-03-06 12:51:13 +0530

    Add files via upload

---
+++ Dice.js
@@ -0,0 +1,16 @@
+let x = Math.random();
+let random = Math.floor((x * 6) + 1);
+let randice = "dice" + random + ".png";
+let psource = "c:/Users/Krishna/Desktop/MERN/" + randice;  
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", psource);
+
+let y = Math.random();
+let random2 = Math.floor((y * 6) + 1);
+let randice2 = "dice" + random2 + ".png";
+let psource2 = "c:/Users/Krishna/Desktop/MERN/" + randice2;  
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", psource2);
+
